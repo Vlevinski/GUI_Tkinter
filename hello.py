f6fd9985d9c53a41080a2c6c484c8f444e3dfec7
Valerian Levinski <v.levinski@gmail.com>
2020-02-21 10:30:11 +0200

    init repo

--- hello.py
+++ hello.py
@@ -1,6 +1,8 @@
 import tkinter as tk
+
+
 class Root(tk.Tk):
-    def __init__(self, items = None):
+    def __init__(self, items=None):
         super().__init__()
         if not items:
             self.items = []
@@ -10,6 +12,9 @@ class Root(tk.Tk):
         self.geometry("300x400")
         self.label = tk.Label(self, text="Hello World", padx=5, pady=5)
         self.label.pack()
+
+
 if __name__ == "__main__":
     root = Root()
+    root.geometry("600x400")
     root.mainloop()
