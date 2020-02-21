4fd56e26f9a3298719f3946f515cba1458df7003
Valerian Levinski <v.levinski@gmail.com>
2020-02-21 11:11:38 +0200

    init repo

--- multi/gui.py
+++ multi/gui.py
@@ -1,6 +1,8 @@
 import tkinter as tk
+
+
 class Root(tk.Tk):
-    def __init__(self, items = None):
+    def __init__(self):
         super().__init__()
         self.title("Regular GUI")
         self.geometry("600x400")
@@ -8,11 +10,11 @@ class Root(tk.Tk):
 
 def main():
     # define two frames
-    left = tk.Frame(root,bg="white")
-    right = tk.Frame(root,bg="grey")
+    left = tk.Frame(root, bg="white")
+    right = tk.Frame(root, bg="grey")
 
-    left.grid(row=0,column=0,sticky="nsew")
-    right.grid(row=0,column=1,sticky="nsew")
+    left.grid(row=0, column=0, sticky="nsew")
+    right.grid(row=0, column=1, sticky="nsew")
 
     # use grid 2x1 (col x row)
     root.rowconfigure(0, weight=1)
@@ -20,9 +22,9 @@ def main():
     root.columnconfigure(1, weight=1)
 
     # show labels
-    label = tk.Label(left, text = "Hello World")
+    label = tk.Label(left, text="Hello World")
     label.pack()
-    label = tk.Label(right, text = "Console Output")
+    label = tk.Label(right, text="Console Output")
     label.pack()
 
 
