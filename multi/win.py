cfe3ceb920d08a9f52bf8ec8fa0e0b45b8295b6e
Valerian Levinski <v.levinski@gmail.com>
2020-02-21 19:14:10 +0200

    init repo

--- multi/win.py
+++ multi/win.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 
 window = tk.Tk()
-label = tk.Label(text="    Python rocks!   ", fg= "white", bg="black")
+label = tk.Label(text="    Python rocks!   ", bg= "#B0C4DE", fg="black",     width=10,    height=10)
 label.pack()
 
 window.mainloop()
