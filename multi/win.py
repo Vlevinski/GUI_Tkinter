a2341a2431a0f2d36a033f595f0a402f49e56499
Valerian Levinski <v.levinski@gmail.com>
2020-02-22 09:50:20 +0200

    init repo

--- multi/win.py
+++ multi/win.py
@@ -15,13 +15,13 @@ class GUI:
 
 class Win1:
 
-    def __init__(self, master):
+    def __init__(self, master, text_one):
         self.master = master
         self.master.geometry("300x400")
         self.master.title("Window")
         self.frame1 = tk.Frame(master)
         self.frame1.pack()
-        self.label_two = tk.Label(master, text=" This is the second window", bg="#FFC0CB")
+        self.label_two = tk.Label(master, text=text_one, bg="#FFC0CB")
         self.label_two.pack()
 
 
@@ -29,9 +29,12 @@ root = tk.Tk()
 app = GUI(root)
 
 win1 = tk.Toplevel(root)
-app1 = Win1(win1)
+app1 = Win1(win1,"First window")
 
 win2 = tk.Toplevel(root)
-app2 = Win1(win2)
+app2 = Win1(win2,"Second window")
+
+win3 = tk.Toplevel(root)
+app3 = Win1(win3,"Third window")
 
 root.mainloop()
