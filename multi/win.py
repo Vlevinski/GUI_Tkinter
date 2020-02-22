adb622849bbf46488a65df090eb8c6770bf98c2c
Valerian Levinski <v.levinski@gmail.com>
2020-02-22 08:30:36 +0200

    init repo

--- multi/win.py
+++ multi/win.py
@@ -5,6 +5,10 @@ class GUI:
 
     def __init__(self, master):
         self.master = master
+        self.master.geometry("300x400")
+        self.master.title(" Master")
+        self.frame = tk.Frame(master)
+        self.frame.pack()
 
 
 root = tk.Tk()
