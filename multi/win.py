346104718723d9a93044ecac8a8ad7c31d6f8b19
Valerian Levinski <v.levinski@gmail.com>
2020-02-22 11:31:26 +0200

    init repo

--- multi/win.py
+++ multi/win.py
@@ -5,11 +5,12 @@ class GUI:
 
     def __init__(self, master):
         self.master = master
-        self.master.geometry("300x400")
+        self.master.geometry("300x400+100+100")
+        self.master.resizable(50, 50)
         self.master.title(" Master")
         self.frame = tk.Frame(master)
         self.frame.pack()
-        self.label_one = tk.Label(text="Click to open new window", bg="#B0C4DE")
+        self.label_one = tk.Label(text="Main window", bg="#B0C4DE")
         self.label_one.pack()
 
 
@@ -17,15 +18,21 @@ class Win1:
 
     def __init__(self, master, text_one):
         self.master = master
-        self.master.geometry("300x400")
-        #        self.master.overrideredirect(1)
-        self.master.resizable(0, 0)
-
+        self.master.geometry("300x400+200+200")
+        self.master.overrideredirect(0)
+        self.master.resizable(10, 10)
         self.master.title("Window")
         self.frame1 = tk.Frame(master)
         self.frame1.pack()
         self.label_two = tk.Label(master, text=text_one, bg="#FFC0CB")
         self.label_two.pack()
+        #        tk.Label(master, text="Press <ESC> to exit").pack()
+        #        master.bind("<Escape>", exit)
+        self.quit = tk.Button(self.master, text="Quit", command=self.close_window)
+        self.quit.pack()
+
+    def close_window(self):
+        self.master.destroy()
 
 
 root = tk.Tk()
@@ -34,10 +41,12 @@ app = GUI(root)
 win1 = tk.Toplevel(root)
 app1 = Win1(win1, "First window")
 
+
 win2 = tk.Toplevel(root)
 app2 = Win1(win2, "Second window")
 
 win3 = tk.Toplevel(root)
 app3 = Win1(win3, "Third window")
+
 
 root.mainloop()
