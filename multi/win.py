6bfcd89c8c7bbe026b5ace3f3f3d0b6928615971
Valerian Levinski <v.levinski@gmail.com>
2020-02-22 08:55:17 +0200

    init repo

--- multi/win.py
+++ multi/win.py
@@ -9,8 +9,16 @@ class GUI:
         self.master.title(" Master")
         self.frame = tk.Frame(master)
         self.frame.pack()
-        self.label_one = tk.Label(text ="Click to open new window")
+        self.label_one = tk.Label(text="Click to open new window")
         self.label_one.pack()
+
+
+class Win1:
+
+    def __init__(self, master):
+        self.master = master
+        self.master.geometry("300x400")
+        self.master.title("Window")
 
 
 root = tk.Tk()
