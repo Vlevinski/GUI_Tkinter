c297b430d08ae246e60c6a9915d7e7d254692127
Valerian Levinski <v.levinski@gmail.com>
2020-02-27 13:39:01 +0200

    init repo

--- multi/win.py
+++ multi/win.py
@@ -1,39 +1,46 @@
 import tkinter as tk
-from multi.gui import GUI,Win1,Tables,Framic, Buttonic, Colorite, Timer
+from multi.gui import GUI, Win1, Tables, Framic, Buttonic, Colorite, Timer
 
 
 root = tk.Tk()
+
+# main window
 app = GUI(root)
 
-# no static windows
+# image
 win1 = tk.Toplevel(root)
 app1 = Win1(win1)
 image = tk.PhotoImage(file='img/plot.png')
 label = tk.Label(win1, image=image)
 label.pack()
 
+# timer
+tm = tk.Toplevel(root)
+app2 = Timer(tm)
+app2.tick()
+
+# frames
+win3 = tk.Toplevel(root)
+app3 = Framic(win3)
+
+# buttons with icons
+win4 = tk.Toplevel(root)
+app4 = Buttonic(win4)
+
+# buttons in color
+win5 = tk.Toplevel(root)
+app5 = Colorite(win5)
+
+# spredsheet
+win6 = tk.Toplevel(root)
+app6 = Tables(win6)
+
+# another plot
 win7 = tk.Toplevel(root)
 app7 = Win1(win7)
 image7 = tk.PhotoImage(file='img/plot1.png')
 label = tk.Label(win7, image=image7)
 label.pack()
-
-tm = tk.Toplevel(root)
-app2 = Timer(tm)
-app2.tick()
-
-win3 = tk.Toplevel(root)
-app3 = Framic(win3)
-
-win4 = tk.Toplevel(root)
-app4 = Buttonic(win4)
-
-
-win5 = tk.Toplevel(root)
-app5 = Colorite(win5)
-
-win6 = tk.Toplevel(root)
-app6 = Tables(win6)
 
 
 root.mainloop()
