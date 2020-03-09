b983132d96f2fe0ec6b698c7953708218b3f541e
Valerian Levinski <v.levinski@gmail.com>
2020-03-09 13:53:09 +0200

    init repo

--- multi/my_form.py
+++ multi/my_form.py
@@ -1,12 +1,34 @@
 import tkinter as tk
 from tkinter.messagebox import showinfo
 
-class MyGui(tk.Frame):
+class MyGui(tk.Tk):
     def __init__(self, parent=None):
-        tk.Frame.__init__(self, parent)
-        self.pack()
-        button = tk.Button(self, text='press', command=self.reply)
-        button.pack()
+        tk.Tk.__init__(self, parent)
+        self.geometry= "500x300"
+        self.title = "Main"
+#        self.pack()
+        self.txt1 = "Welcome Back !\n"
+        self.txt2 = "To keep connected with us please login \n with your personal info"
+        # frames
+        frm1 = tk.Frame(self, bg="blue")
+        frm2 = tk.Frame(self, bg="white")
+        topl = tk.Frame(frm1, bg="brown1")
+        botl = tk.Frame(frm1, bg="brown1")
+
+        # frames packs
+        frm1.pack(side=tk.LEFT, expand=tk.YES, fill=tk.BOTH)
+        frm2.pack(side=tk.LEFT, expand=tk.YES, fill=tk.BOTH)
+        topl.pack(side=tk.TOP, expand=tk.YES, fill=tk.BOTH)
+        botl.pack(side=tk.BOTTOM, expand=tk.YES, fill=tk.BOTH)
+
+        # left
+        lbl1 = tk.Label(topl, text=self.txt1, font=('helvetica', 15, 'bold'), fg="white", bg="brown1").pack(side=tk.BOTTOM)
+        lbl2 = tk.Label(botl, text=self.txt2, fg="white", bg="brown1").pack()
+
+        # buttons
+        btn1 = tk.Button(botl, text="SIGN IN", fg="brown1", bg="white", command=lambda: self.reply()).pack(expand=tk.YES)
+        btn2 = tk.Button(frm2, text="SIGN UP", command=lambda: self.reply()).pack(expand=tk.YES)
+
 
     def reply(self):
         showinfo(title='popup', message='Button pressed!')
@@ -16,8 +38,5 @@ class MyWin(MyGui):
         MyGui.__init__(self)
         lbl = tk.Label(self, text="Hello").pack()
 
-root=tk.Tk()
-mygui= MyGui(root)
-mywin= MyWin()
 
-root.mainloop()
+mygui= MyGui().mainloop()
