050a3fe9a874747ab561b2912b23aa11860096a3
Valerian Levinski <v.levinski@gmail.com>
2020-03-14 10:37:24 +0200

    init repo

--- widgets/hello.py
+++ widgets/hello.py
@@ -2,13 +2,12 @@ import tkinter as tk
 
 
 class Root(tk.Tk):
-    def __init__(self, items=None):
+    def __init__(self):
         super().__init__()
         self.title("GUI")
         self.geometry("300x400")
         self.label = tk.Label(self, text="Desktop", fg="white", padx=5, pady=5)
         self.label.pack()
-
 
 
 if __name__ == "__main__":
