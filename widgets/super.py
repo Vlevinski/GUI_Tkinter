94dd645661d6aba37824be2018ace2b79e9eaf8d
Valerian Levinski <v.levinski@gmail.com>
2020-03-14 10:34:51 +0200

    init repo

--- widgets/super.py
+++ widgets/super.py
@@ -1,12 +1,1 @@
-import tkinter as tk
-
-
-class Application(tk.Frame):
-    def __init__(self, parent):
-        super().__init__(parent)
-        tk.Button(self, text='Super!', command=root.destroy).pack()
-
-
-root = tk.Tk()
-Application(root).pack()
-root.mainloop()
+#
