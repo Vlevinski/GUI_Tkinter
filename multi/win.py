cf3bb4376f5947cee3bfcf6d24f6b67e11b03904
Valerian Levinski <v.levinski@gmail.com>
2020-03-14 10:55:33 +0200

    init repo

--- multi/win.py
+++ multi/win.py
@@ -1,9 +1,9 @@
 import tkinter as tk
-from multi.gui import GUI, Win1, Tables, Framic, Buttonic, Colorite, Timer
+#from multi.gui import GUI, Win1, Tables, Framic, Buttonic, Colorite, Timer
 
 root = tk.Tk()
 
 # main window
-app = GUI(root)
+#app = GUI(root)
 
 root.mainloop()
